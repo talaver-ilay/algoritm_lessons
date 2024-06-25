c7a5d6f544a0af97706c22ea8459d058cf0ed401
linux_home <varenandrej@gmail.com>
2024-06-25 22:37:34 +0300

    add hash table

---
+++ hashTable.py
@@ -0,0 +1,14 @@
+# book = dict() # пустая хеш-таблица
+# book["apple"] = 0.67
+# book["milk"] = 1.49
+# book["avocado"] = 1.49
+# print(book)
+# print(book["avocado"])
+
+voted = {} # or phone_book = dict()
+value = voted.get("tom")
+print (value)
+voted["tom"] = 12
+value = voted.get("tom")
+print (value)
+
